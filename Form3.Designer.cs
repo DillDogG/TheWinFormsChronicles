c8eebc82082b9b4644fb70251dc9a622bc236e56
DillDogG <dilldoggaming@gmail.com>
2023-12-03 19:02:31 -0700

    Update

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -60,62 +60,69 @@
             // pictureBox1
             // 
             pictureBox1.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox1.Location = new Point(100, 185);
+            pictureBox1.Location = new Point(70, 111);
+            pictureBox1.Margin = new Padding(2, 2, 2, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(200, 200);
+            pictureBox1.Size = new Size(141, 121);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
             pictureBox2.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox2.Location = new Point(350, 185);
+            pictureBox2.Location = new Point(245, 111);
+            pictureBox2.Margin = new Padding(2, 2, 2, 2);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(200, 200);
+            pictureBox2.Size = new Size(141, 121);
             pictureBox2.TabIndex = 1;
             pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
             pictureBox3.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox3.Location = new Point(850, 185);
+            pictureBox3.Location = new Point(595, 111);
+            pictureBox3.Margin = new Padding(2, 2, 2, 2);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(200, 200);
+            pictureBox3.Size = new Size(141, 121);
             pictureBox3.TabIndex = 3;
             pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
             pictureBox4.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox4.Location = new Point(600, 185);
+            pictureBox4.Location = new Point(420, 111);
+            pictureBox4.Margin = new Padding(2, 2, 2, 2);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(200, 200);
+            pictureBox4.Size = new Size(141, 121);
             pictureBox4.TabIndex = 2;
             pictureBox4.TabStop = false;
             // 
             // pictureBox5
             // 
             pictureBox5.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox5.Location = new Point(1350, 185);
+            pictureBox5.Location = new Point(945, 111);
+            pictureBox5.Margin = new Padding(2, 2, 2, 2);
             pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(200, 200);
+            pictureBox5.Size = new Size(141, 121);
             pictureBox5.TabIndex = 5;
             pictureBox5.TabStop = false;
             // 
             // pictureBox6
             // 
             pictureBox6.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox6.Location = new Point(1100, 185);
+            pictureBox6.Location = new Point(770, 111);
+            pictureBox6.Margin = new Padding(2, 2, 2, 2);
             pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(200, 200);
+            pictureBox6.Size = new Size(141, 121);
             pictureBox6.TabIndex = 4;
             pictureBox6.TabStop = false;
             // 
             // charDisp
             // 
-            charDisp.Location = new Point(753, 617);
+            charDisp.Location = new Point(527, 370);
+            charDisp.Margin = new Padding(2, 2, 2, 2);
             charDisp.Name = "charDisp";
-            charDisp.Size = new Size(149, 34);
+            charDisp.Size = new Size(104, 20);
             charDisp.TabIndex = 6;
             charDisp.Text = "View Character";
             charDisp.UseVisualStyleBackColor = true;
@@ -123,137 +130,157 @@
             // 
             // Attack
             // 
-            Attack.Location = new Point(644, 429);
+            Attack.Location = new Point(451, 257);
+            Attack.Margin = new Padding(2, 2, 2, 2);
             Attack.Name = "Attack";
-            Attack.Size = new Size(112, 34);
+            Attack.Size = new Size(78, 20);
             Attack.TabIndex = 7;
             Attack.Text = "Attack";
             Attack.UseVisualStyleBackColor = true;
             // 
             // Shoot
             // 
-            Shoot.Location = new Point(894, 429);
+            Shoot.Location = new Point(626, 257);
+            Shoot.Margin = new Padding(2, 2, 2, 2);
             Shoot.Name = "Shoot";
-            Shoot.Size = new Size(112, 34);
+            Shoot.Size = new Size(78, 20);
             Shoot.TabIndex = 8;
             Shoot.Text = "Shoot";
             Shoot.UseVisualStyleBackColor = true;
             // 
             // Run
             // 
-            Run.Location = new Point(894, 479);
+            Run.Location = new Point(626, 287);
+            Run.Margin = new Padding(2, 2, 2, 2);
             Run.Name = "Run";
-            Run.Size = new Size(112, 34);
+            Run.Size = new Size(78, 20);
             Run.TabIndex = 10;
             Run.Text = "Run";
             Run.UseVisualStyleBackColor = true;
             // 
             // Defend
             // 
-            Defend.Location = new Point(644, 479);
+            Defend.Location = new Point(451, 287);
+            Defend.Margin = new Padding(2, 2, 2, 2);
             Defend.Name = "Defend";
-            Defend.Size = new Size(112, 34);
+            Defend.Size = new Size(78, 20);
             Defend.TabIndex = 9;
             Defend.Text = "Defend";
             Defend.UseVisualStyleBackColor = true;
             // 
             // Move1
             // 
-            Move1.Location = new Point(150, 125);
+            Move1.Enabled = false;
+            Move1.Location = new Point(105, 75);
+            Move1.Margin = new Padding(2, 2, 2, 2);
             Move1.Name = "Move1";
-            Move1.Size = new Size(112, 34);
+            Move1.Size = new Size(78, 20);
             Move1.TabIndex = 11;
             Move1.Text = "Move Here";
             Move1.UseVisualStyleBackColor = true;
             // 
             // Move2
             // 
-            Move2.Location = new Point(400, 125);
+            Move2.Enabled = false;
+            Move2.Location = new Point(280, 75);
+            Move2.Margin = new Padding(2, 2, 2, 2);
             Move2.Name = "Move2";
-            Move2.Size = new Size(112, 34);
+            Move2.Size = new Size(78, 20);
             Move2.TabIndex = 12;
             Move2.Text = "Move Here";
             Move2.UseVisualStyleBackColor = true;
             // 
             // Move3
             // 
-            Move3.Location = new Point(650, 125);
+            Move3.Enabled = false;
+            Move3.Location = new Point(455, 75);
+            Move3.Margin = new Padding(2, 2, 2, 2);
             Move3.Name = "Move3";
-            Move3.Size = new Size(112, 34);
+            Move3.Size = new Size(78, 20);
             Move3.TabIndex = 13;
             Move3.Text = "Move Here";
             Move3.UseVisualStyleBackColor = true;
             // 
             // Move6
             // 
-            Move6.Location = new Point(1394, 125);
+            Move6.Enabled = false;
+            Move6.Location = new Point(976, 75);
+            Move6.Margin = new Padding(2, 2, 2, 2);
             Move6.Name = "Move6";
-            Move6.Size = new Size(112, 34);
+            Move6.Size = new Size(78, 20);
             Move6.TabIndex = 16;
             Move6.Text = "Move Here";
             Move6.UseVisualStyleBackColor = true;
             // 
             // Move5
             // 
-            Move5.Location = new Point(1144, 125);
+            Move5.Enabled = false;
+            Move5.Location = new Point(801, 75);
+            Move5.Margin = new Padding(2, 2, 2, 2);
             Move5.Name = "Move5";
-            Move5.Size = new Size(112, 34);
+            Move5.Size = new Size(78, 20);
             Move5.TabIndex = 15;
             Move5.Text = "Move Here";
             Move5.UseVisualStyleBackColor = true;
             // 
             // Move4
             // 
-            Move4.Location = new Point(894, 125);
+            Move4.Enabled = false;
+            Move4.Location = new Point(626, 75);
+            Move4.Margin = new Padding(2, 2, 2, 2);
             Move4.Name = "Move4";
-            Move4.Size = new Size(112, 34);
+            Move4.Size = new Size(78, 20);
             Move4.TabIndex = 14;
             Move4.Text = "Move Here";
             Move4.UseVisualStyleBackColor = true;
             // 
             // WeapSkill
             // 
-            WeapSkill.Location = new Point(884, 539);
+            WeapSkill.Location = new Point(619, 323);
+            WeapSkill.Margin = new Padding(2, 2, 2, 2);
             WeapSkill.Name = "WeapSkill";
-            WeapSkill.Size = new Size(133, 34);
+            WeapSkill.Size = new Size(93, 20);
             WeapSkill.TabIndex = 18;
             WeapSkill.Text = "Weapon Skill";
             WeapSkill.UseVisualStyleBackColor = true;
             // 
             // Force
             // 
-            Force.Location = new Point(644, 539);
+            Force.Location = new Point(451, 323);
+            Force.Margin = new Padding(2, 2, 2, 2);
             Force.Name = "Force";
-            Force.Size = new Size(112, 34);
+            Force.Size = new Size(78, 20);
             Force.TabIndex = 17;
             Force.Text = "Force";
             Force.UseVisualStyleBackColor = true;
             // 
             // phpDisp
             // 
-            phpDisp.Location = new Point(376, 429);
+            phpDisp.Location = new Point(263, 257);
+            phpDisp.Margin = new Padding(2, 2, 2, 2);
             phpDisp.Name = "phpDisp";
             phpDisp.ReadOnly = true;
-            phpDisp.Size = new Size(150, 31);
+            phpDisp.Size = new Size(106, 23);
             phpDisp.TabIndex = 19;
             phpDisp.Text = "Health: ";
             phpDisp.TextAlign = HorizontalAlignment.Center;
             // 
             // ehpDisp
             // 
-            ehpDisp.Location = new Point(1108, 429);
+            ehpDisp.Location = new Point(776, 257);
+            ehpDisp.Margin = new Padding(2, 2, 2, 2);
             ehpDisp.Name = "ehpDisp";
             ehpDisp.ReadOnly = true;
-            ehpDisp.Size = new Size(188, 31);
+            ehpDisp.Size = new Size(133, 23);
             ehpDisp.TabIndex = 20;
             ehpDisp.Text = "Enemy Health: ";
             ehpDisp.TextAlign = HorizontalAlignment.Center;
             // 
             // Form3
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1678, 764);
+            ClientSize = new Size(1175, 458);
             Controls.Add(ehpDisp);
             Controls.Add(phpDisp);
             Controls.Add(WeapSkill);
@@ -275,6 +302,7 @@
             Controls.Add(pictureBox4);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form3";
             Text = "Form3";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
